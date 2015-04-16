c709377e7a196e92fda37bbc10984944ac7eabd6
Simon Martinelli <sm@simas.ch>
2015-04-16 20:35:01 +0200

    added constructor query

--- webapp/src/main/java/erp/customer/boundry/CustomerBean.java
+++ webapp/src/main/java/erp/customer/boundry/CustomerBean.java
@@ -24,6 +24,7 @@ public class CustomerBean {
     }
 
     public void searchV2() {
+        customers = customerService.getCustomersWithConstructorExpression(searchText);
     }
 
     public void clear() {
--- webapp/src/main/java/erp/customer/control/CustomerService.java
+++ webapp/src/main/java/erp/customer/control/CustomerService.java
@@ -1,10 +1,12 @@
 package erp.customer.control;
 
 import erp.customer.entity.Customer;
+import erp.customer.entity.CustomerInfoDTO;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,8 +19,19 @@ public class CustomerService {
 
     public List<Customer> getCustomers(String term) {
         TypedQuery<Customer> q = em.createQuery(
-                "SELECT c FROM Customer c WHERE c.lastname LIKE :term ORDER BY c.lastname, c.firstname", 
+                "SELECT c FROM Customer c WHERE c.lastname LIKE :term ORDER BY c.lastname, c.firstname",
                 Customer.class);
+        q.setParameter("term", term + "%");
+        return q.getResultList();
+    }
+
+    public List<CustomerInfoDTO> getCustomersWithConstructorExpression(String term) {
+        Query q = em.createQuery(
+                "SELECT NEW erp.customer.entity.CustomerInfoDTO(c.id, c.lastname, c.firstname, SUM(i.product.price)) "
+                + "FROM Customer c JOIN c.orders o JOIN o.items i "
+                + "WHERE c.lastname LIKE :term "
+                + "GROUP BY c.id, c.lastname, c.firstname "
+                + "ORDER BY c.lastname, c.firstname");
         q.setParameter("term", term + "%");
         return q.getResultList();
     }
