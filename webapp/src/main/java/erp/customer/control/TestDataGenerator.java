09a0744db93334a2193ce9b046cfaacd15b8d1ff
simasch <smtve,01>
2016-12-11 18:08:01 +0100

    changed random data generator

--- webapp/src/main/java/erp/customer/control/TestDataGenerator.java
+++ webapp/src/main/java/erp/customer/control/TestDataGenerator.java
@@ -4,13 +4,17 @@ import erp.customer.entity.Customer;
 import erp.customer.entity.Order;
 import erp.customer.entity.OrderItem;
 import erp.product.entity.Product;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import org.soqqo.datagen.RandomDataGenerator;
-import org.soqqo.datagen.config.DataTypes.Name;
-import org.soqqo.datagen.config.GenConfig;
+
+import io.codearte.jfairy.Fairy;
+import io.codearte.jfairy.producer.DateProducer;
+import io.codearte.jfairy.producer.person.Person;
+import org.joda.time.LocalDate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,32 +26,38 @@ public class TestDataGenerator {
 
     @Transactional
     public void generate() {
+        Fairy fairy = Fairy.create();
+
         Random random = new Random();
         Product product = new Product();
         product.setName("Bleistift");
         product.setPrice(2.5d);
         em.persist(product);
 
-        RandomDataGenerator rdg = new RandomDataGenerator();
-        List<Customer> customers = rdg.generateList(400,
-                new GenConfig()
-                .name(Name.Firstname, "firstname")
-                .name(Name.Lastname, "lastname"), Customer.class);
-
-        for (Customer customer : customers) {
+        List<Customer> customers = new ArrayList<>();
+        for (int i = 0; i < 400; i++) {
+            Person person = fairy.person();
+            Customer customer = new Customer();
+            customer.setFirstname(person.getFirstName());
+            customer.setLastname(person.getLastName());
             em.persist(customer);
+            customers.add(customer);
+        }
 
-            List<Order> orders = rdg.generateList(random.nextInt(10) + 1,
-                    new GenConfig()
-                    .dateTimeRandom("orderDate"), Order.class);
+        DateProducer dateProducer = fairy.dateProducer();
+        for (Customer customer : customers) {
+            List<Order> orders = new ArrayList<>();
+            for (int i = 0; i < random.nextInt(10) + 1; i++) {
+                Order order = new Order();
+                order.setOrderDate(dateProducer.randomDateInThePast(10).toDate());
+                customer.getOrders().add(order);
+            }
 
             for (Order order : orders) {
-                customer.getOrders().add(order);
+                List<OrderItem> orderItems = new ArrayList<>();
 
-                List<OrderItem> orderItems = rdg.generateList(random.nextInt(20) + 1,
-                        new GenConfig(), OrderItem.class);
-
-                for (OrderItem item : orderItems) {
+                for (int i = 0; i < random.nextInt(20) + 1; i++) {
+                    OrderItem item = new OrderItem();
                     item.setProduct(product);
                     item.setQuantity(random.nextInt(5) + 1);
 
